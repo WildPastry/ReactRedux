68635ecdb5bf25dec5cc2c4af765d311d09f5d4d
WildPastry <mykdsn@gmail.com>
2019-04-21 12:01:17 +1200

    Initial setup commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from "./inc/Menu";
+
+import './scss/main.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+      <p>mike parker | portfolio</p>
+      <Menu />
+      </React.Fragment>
     );
   }
 }
