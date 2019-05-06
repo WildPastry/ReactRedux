c885f9199bdaa6815a4637c5e0fd43847b9d259f
WildPastry <mykdsn@gmail.com>
2019-05-07 09:12:43 +1200

    random projects 100% complete

--- src/inc/Project.tsx
+++ src/inc/Project.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+var randomNumber: any[] | number[] | (string | number)[];
+
 class Project extends Component<any, any> {
   constructor(props: any) {
     super(props);
@@ -21,7 +23,16 @@ class Project extends Component<any, any> {
     this.prevProject = this.prevProject.bind(this);
     this.nextProject = this.nextProject.bind(this);
     this.changeProject = this.changeProject.bind(this);
+    this.moreProjects = this.moreProjects.bind(this);
     this.changePageFromProject = this.changePageFromProject.bind(this);
+  }
+
+  randomProject() {
+    randomNumber = [];
+    while (randomNumber.length < 3) {
+      var r = Math.floor(Math.random() * 12);
+      if (randomNumber.indexOf(r) === -1) randomNumber.push(r);
+    }
   }
 
   changeProject() {
@@ -41,6 +52,7 @@ class Project extends Component<any, any> {
   }
 
   prevProject() {
+    this.randomProject();
     window.scrollTo(0, 0);
     var i;
     if (this.state.currentProject === '001') {
@@ -84,6 +96,7 @@ class Project extends Component<any, any> {
   }
 
   nextProject() {
+    this.randomProject();
     window.scrollTo(0, 0);
     var i;
     if (this.state.currentProject === '001') {
@@ -126,14 +139,62 @@ class Project extends Component<any, any> {
     });
   }
 
+  moreProjects(value: any) {
+    this.randomProject();
+    window.scrollTo(0, 0);
+    console.log(value);
+    var j;
+    if (value === 1) {
+      j = 1;
+    } else if (value === 2) {
+      j = 2;
+    } else if (value === 3) {
+      j = 3;
+    } else if (value === 4) {
+      j = 4;
+    } else if (value === 5) {
+      j = 5;
+    } else if (value === 6) {
+      j = 6;
+    } else if (value === 7) {
+      j = 7;
+    } else if (value === 8) {
+      j = 8;
+    } else if (value === 9) {
+      j = 9;
+    } else if (value === 10) {
+      j = 10;
+    } else if (value === 11) {
+      j = 11;
+    } else if (value === 12) {
+      j = 12;
+    }
+    this.setState({
+      currentProject: this.state.projects[j].id,
+      currentProjectName: this.state.projects[j].name,
+      currentProjectFields: this.state.projects[j].fields,
+      currentProjectClient: this.state.projects[j].client,
+      currentProjectTimeline: this.state.projects[j].timeline,
+      currentProjectWebsite: this.state.projects[j].website,
+      currentProjectThumb: this.state.projects[j].thumb,
+      currentProjectImages: this.state.projects[j].images,
+      currentProjectIcons: this.state.projects[j].icons,
+      currentProjectIntro: this.state.projects[j].intro,
+      currentProjectDesc: this.state.projects[j].desc
+    });
+  }
+
   changePageFromProject(value: any) {
     window.scrollTo(0, 0);
     this.props.changePageFromProject(value);
   }
 
+  componentWillMount() {
+    this.randomProject();
+  }
+
   componentDidMount() {
     console.log('Project component loaded...');
-    console.log(this.props.projectDetails);
   }
 
   render() {
@@ -198,7 +259,7 @@ class Project extends Component<any, any> {
             <div className='colWrap col-xs-12 col-sm-12 col-md-12 col-lg-12'>
               <div className='lineThin mar' />
             </div>
-            {/* PROJECT CONTROL ICONS */}
+            {/* TOP PROJECT CONTROL ICONS */}
             <div className='colWrap flex wrap col-xs-12 col-sm-12 col-md-12 col-lg-12'>
               <div className='iconWrap' onClick={this.prevProject}>
                 <FontAwesomeIcon icon={['fas', 'chevron-left']} className='prevIcon' />
@@ -229,8 +290,8 @@ class Project extends Component<any, any> {
             </div>
           </div>
         </div>
-        {/* PROJECT CONTROL ICONS */}
-        <div className='container-fluid'>
+        {/* BOTTOM PROJECT CONTROL ICONS */}
+        {/* <div className='container-fluid'>
           <div className='colWrap flex wrap col-xs-12 col-sm-12 col-md-12 col-lg-12'>
             <div className='iconWrap' onClick={this.prevProject}>
               <FontAwesomeIcon icon={['fas', 'chevron-left']} className='prevIcon' />
@@ -240,6 +301,38 @@ class Project extends Component<any, any> {
             </div>
             <div className='iconWrap' onClick={this.nextProject}>
               <FontAwesomeIcon icon={['fas', 'chevron-right']} className='nextIcon' />
+            </div>
+          </div>
+        </div> */}
+        {/* MORE PROJECTS */}
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='moreProjectsWrap col-xs-12 col-sm-12 col-md-12 col-lg-12'>
+              <h3 className='center marBotProject textPeach'>more projects</h3>
+            </div>
+            <div className='imgWrapDark col-xs-12 col-sm-6 col-md-4 col-lg-4'>
+              <img
+                onClick={this.moreProjects.bind(this, randomNumber[0])}
+                src={require('./../img/thumb/' +
+                  this.state.projects[randomNumber[0]].thumb[Math.round(Math.random())])}
+                alt='Gallery'
+              />
+            </div>
+            <div className='imgWrapDark col-xs-12 col-sm-6 col-md-4 col-lg-4'>
+              <img
+                onClick={this.moreProjects.bind(this, randomNumber[1])}
+                src={require('./../img/thumb/' +
+                  this.state.projects[randomNumber[1]].thumb[Math.round(Math.random())])}
+                alt='Gallery'
+              />
+            </div>
+            <div className='imgWrapDark col-xs-12 col-sm-6 col-md-4 col-lg-4'>
+              <img
+                onClick={this.moreProjects.bind(this, randomNumber[2])}
+                src={require('./../img/thumb/' +
+                  this.state.projects[randomNumber[2]].thumb[Math.round(Math.random())])}
+                alt='Gallery'
+              />
             </div>
           </div>
         </div>
