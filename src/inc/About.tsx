9a7cddf89e0702e31553c6b749ea922f68d31a87
WildPastry <mykdsn@gmail.com>
2019-05-14 10:25:08 +1200

    Padding adjustments

--- src/inc/About.tsx
+++ src/inc/About.tsx
@@ -8,7 +8,7 @@ class About extends Component<any, any> {
     super(props);
     this.space = React.createRef();
   }
-  
+
   componentDidMount() {
     initSpace();
   }
@@ -24,9 +24,9 @@ class About extends Component<any, any> {
             </div>
             <div className='colWrap col-xs-12 col-sm-12 col-md-6 col-lg-4'>
               <p className={this.props.aboutPara}>
-                Experienced Web and Graphic Designer from Christchurch, New Zealand. Currently
-                upskilling further in Web and UX development at Yoobee School of Design and have
-                included a recent selection of work on my site.
+                Javascript, PHP, and React Web Developer from Christchurch, New Zealand. Looking for
+                a full-time position in a digital agency, tech company, or as part of an in-house
+                development team.
                 <br />
                 <br /> If you have any questions, would like to know more information about me or
                 any of my projects, or would like to work together on a future project, give me a
