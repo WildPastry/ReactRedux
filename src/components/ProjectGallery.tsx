737d0dd2941b7c5ca3570ae56b9d7acad975df3f
WildPastry <mykdsn@gmail.com>
2022-09-21 07:35:10 +1200

    Fixing eslint warnings. Updating rules.

--- src/components/ProjectGallery.tsx
+++ src/components/ProjectGallery.tsx
@@ -1,50 +1,49 @@
-// imports
-import React from 'react';
-import { Row } from 'react-bootstrap';
+import { Project, ProjectFilter } from '../types';
+import { useDispatch, useSelector } from 'react-redux';
 import ProjectItem from './ProjectItem';
+import React from 'react';
 import { RootState } from '../redux/reducers/rootReducer';
-import { useSelector, useDispatch } from 'react-redux';
+import { Row } from 'react-bootstrap';
 import { setFilters } from '../redux/slices/projectSlice';
-import { ProjectFilter } from '../types';
-import { Project } from '../types';
 
-// getfilteredProjects function
-const getfilteredProjects = (projects: Project[], filter: ProjectFilter) => {
-	switch (filter) {
-		case ProjectFilter.ShowAll:
-			return projects;
-		case ProjectFilter.ShowApps:
-			return projects.filter((proj) => proj.type.indexOf('APP') >= 0);
-		case ProjectFilter.ShowBranding:
-			return projects.filter((proj) => proj.type.indexOf('BRANDING') >= 0);
-		case ProjectFilter.ShowGraphic:
-			return projects.filter((proj) => proj.type.indexOf('GRAPHIC') >= 0);
-		case ProjectFilter.ShowMarketing:
-			return projects.filter((proj) => proj.type.indexOf('MARKETING') >= 0);
-		case ProjectFilter.ShowWebsites:
-			return projects.filter((proj) => proj.type.indexOf('WEBSITE') >= 0);
-		default:
-			throw new Error('Unknown filter: ' + filter);
-	}
+// GetfilteredProjects function
+const getfilteredProjects = (
+  projects: Project[],
+  filter: ProjectFilter
+): Project[] => {
+  switch (filter) {
+  case ProjectFilter.ShowAll:
+    return projects;
+  case ProjectFilter.ShowApps:
+    return projects.filter((proj) => proj.type.indexOf('APP') >= 0);
+  case ProjectFilter.ShowBranding:
+    return projects.filter((proj) => proj.type.indexOf('BRANDING') >= 0);
+  case ProjectFilter.ShowGraphic:
+    return projects.filter((proj) => proj.type.indexOf('GRAPHIC') >= 0);
+  case ProjectFilter.ShowMarketing:
+    return projects.filter((proj) => proj.type.indexOf('MARKETING') >= 0);
+  case ProjectFilter.ShowWebsites:
+    return projects.filter((proj) => proj.type.indexOf('WEBSITE') >= 0);
+  default:
+    throw new Error(`Unknown filter: ${filter}`);
+  }
 };
 
 // ProjectList function
-export default function ProjectList() {
-	const dispatch = useDispatch();
+export default function ProjectList(): JSX.Element {
+  const dispatch = useDispatch();
 
-	const projects = useSelector((state: RootState) =>
-		getfilteredProjects(state.setProjects, state.setFilters)
-	);
+  const projects = useSelector((state: RootState): Project[] => getfilteredProjects(state.setProjects, state.setFilters));
 
-	return (
-		<Row>
-			{projects.map((project) => (
-				<ProjectItem
-					key={project.id}
-					{...project}
-					onClick={() => dispatch(setFilters(project))}
-				/>
-			))}
-		</Row>
-	);
+  return (
+    <Row>
+      {projects.map((project) => (
+        <ProjectItem
+          key={project.id}
+          {...project}
+          onClick={() => dispatch(setFilters(project))}
+        />
+      ))}
+    </Row>
+  );
 }
--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,59 +1,57 @@
-// imports
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../redux/reducers/rootReducer';
 import { setNav, setProject } from '../redux/slices/navSlice';
+import { useDispatch, useSelector } from 'react-redux';
 import { Col } from 'react-bootstrap';
 import { MDBAnimation } from 'mdbreact';
 import { Project } from '../types';
+import React from 'react';
+import { RootState } from '../redux/reducers/rootReducer';
 
 // ProjectItem function
 export default function ProjectItem({
-	id,
-	fields,
-	name,
-	thumb
+  id,
+  fields,
+  name,
+  thumb
 }: Project): JSX.Element {
-	// useSelectors for app and img themes
-	const appTheme = useSelector((state: RootState) => {
-		return state.setTheme.appTheme;
-	});
+  // UseSelectors for app and img themes
+  const appTheme = useSelector((state: RootState): string => {
+    return state.setTheme.appTheme;
+  });
 
-	// dispatch
-	const dispatch = useDispatch();
+  // Dispatch
+  const dispatch = useDispatch();
 
-	// handle function for navigation
-	const handleNav = (section: string, id: number) => {
-		// set current project using ID
-		// then navigate to correct section and scroll to top
-		dispatch(setProject(id));
-		dispatch(setNav(section));
-		window.scrollTo(0, 0);
-	};
+  // Handle function for navigation
+  const handleNav = (section: string, id: number): void => {
+    // Set current project using ID then navigate to correct section and scroll to top
+    dispatch(setProject(id));
+    dispatch(setNav(section));
+    window.scrollTo(0, 0);
+  };
 
-	return (
-		<React.Fragment>
-			<Col xs={12} sm={6} md={4}>
-				<MDBAnimation type='zoomIn'>
-					<img
-						aria-label='Project Gallery Image'
-						src={require('./../img/thumb/' + thumb[0])}
-						onClick={() => handleNav('PROJECT', id)}
-						className={'imgWrap'}
-						alt={name}
-					/>
-				</MDBAnimation>
-				<div aria-label='Project Gallery Description' className='descriptionWrap'>
-					<p className={'text700 textMed' + appTheme}>{name}</p>
-					<p className={'text400 textFlip' + appTheme}>
-						{fields[0]}
-						<span className='textLightGrey text400'> / </span>
-						{fields[1]}
-						<span className='textLightGrey text400'> / </span>
-						{fields[2]}
-					</p>
-				</div>
-			</Col>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <Col xs={12} sm={6} md={4}>
+        <MDBAnimation type="zoomIn">
+          <img
+            aria-label="Project Gallery Image"
+            src={require(`./../img/thumb/${thumb[0]}`)}
+            onClick={() => handleNav('PROJECT', id)}
+            className={'imgWrap'}
+            alt={name}
+          />
+        </MDBAnimation>
+        <div aria-label="Project Gallery Description" className="descriptionWrap">
+          <p className={`text700 textMed${appTheme}`}>{name}</p>
+          <p className={`text400 textFlip${appTheme}`}>
+            {fields[0]}
+            <span className="textLightGrey text400"> / </span>
+            {fields[1]}
+            <span className="textLightGrey text400"> / </span>
+            {fields[2]}
+          </p>
+        </div>
+      </Col>
+    </React.Fragment>
+  );
 }
