742a12265db27c590e55f396b38e5b542fd763ee
WildPastry <mykdsn@gmail.com>
2022-09-25 19:03:19 +1300

    Adjusting rules

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,9 +1,7 @@
-/// <reference types="react-scripts" />
-
-// declarations
-declare var require: {
+// Declarations
+declare let require: {
   <T>(path: string): T;
-  (paths: string[], callback: (...modules: any[]) => void): void;
+  (paths: string[], callback: (...modules) => void): void;
   ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
 };
 
